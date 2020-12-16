4a1c4ec83edc0a3ae6b14caaeab27265d0b70105
Yuki-Naka-Sharing-Study <yukinaka2168@au.com>
2020-12-16 16:47:38 +0900

    【《課題》あいさつアプリ】の原型作成

--- app/src/main/java/jp/techacademy/yuki/naka/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/naka/aisatsuapp/MainActivity.kt
@@ -1,11 +1,37 @@
 package jp.techacademy.yuki.naka.aisatsuapp
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+        button3.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button1) {
+            textView.text = editText.text.toString()
+        } else if (v.id == R.id.button3) {
+            showTimePickerDialog()
+        }
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+                this,
+                TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                    Log.d("UI_PARTS", "$hour:$minute")
+                },
+                13, 0, true)
+        timePickerDialog.show()
     }
 }
