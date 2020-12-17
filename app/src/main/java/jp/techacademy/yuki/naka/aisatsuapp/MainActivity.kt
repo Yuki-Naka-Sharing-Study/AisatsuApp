1ff3abc1f755f392b6e98e2ec1e65ca09b5c184a
Yuki-Naka-Sharing-Study <yukinaka2168@au.com>
2020-12-17 16:53:10 +0900

    【《課題》あいさつアプリ】『if (2:00 ~ 9:59, 0, true) {』を『if (2:00 <= 9:59, 0, true) {』に変更 ⇒ エラー発生

--- app/src/main/java/jp/techacademy/yuki/naka/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuki/naka/aisatsuapp/MainActivity.kt
@@ -28,11 +28,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 TimePickerDialog.OnTimeSetListener { view, hour, minute ->
                     Log.d("UI_PARTS", "$hour:$minute")
                 },
-                if (2:00 ~ 9:59, 0, true) {
+                if (2:00 <= 9:59, 0, true) {
             　　　　 textView.text = "おはよう"
-                } else if (10:00 ~ 17:59, 0, true) {
+                } else if (10:00 <= 17:59, 0, true) {
             　　　　 textView.text = "こんにちは"
-                } else (18:00 ~ 1:59, 0, true){
+                } else (18:00 <= 1:59, 0, true){
             　　　　 textView.text = "こんばんは"
                 }
                 timePickerDialog.show()
